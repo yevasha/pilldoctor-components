2f9c0ddabba77a9ca4c38f4ae18ecb4db80e8b98
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-09-18 15:02:37 +0200

    Register start

---
+++ src/stories/Components/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Email from "./Email";
+
+const Register = () => {
+    return (
+        <div>
+            register
+            <Email />
+        </div>
+    )
+}
+
+export default Register;
---
+++ src/stories/Components/Register/Register.stories.jsx
@@ -0,0 +1,10 @@
+import Register from "./Register";
+
+export default {
+    title: 'FRK/Components/register',
+    component: Register
+}
+
+export const Default = {
+    
+}
