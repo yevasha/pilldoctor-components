cbf9236ddd4352e2bceb682f9f340b23108f9637
Anna Olsson <annapolsson@hotmail.com>
2024-09-18 14:13:12 +0200

    calender

---
+++ src/stories/Components/PillCalendar/Calender.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "Calender.modules.css";
+
+const Calender = () => {
+    return (
+        <div className="Outer-box">
+        </div>
+    );
+}
+
+export default Calender;
