4e7b48abe7965b485ae8080556c55b808b2369ed
samina-shameem <samina.shameem.clt@gmail.com>
2024-09-18 14:00:29 +0200

    created profile page and page for bookstory

---
+++ src/stories/Components/Profile/ProfilePage.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProfilePage from './ProfilePage';
+
+export default {
+  title: 'PillDoctor/ProfilePage',
+  component: ProfilePage,
+};
+
+const Template = (args) => <ProfilePage {...args} />;
+
+export const Default = Template.bind({});
