6f2c4f99315d90557caebc8082de7879b2e670df
Anna Olsson <annapolsson@hotmail.com>
2024-09-23 11:06:49 +0200

    added styling to reminder

--- src/stories/Components/PillCalendar/Reminder.jsx
+++ src/stories/Components/PillCalendar/Reminder.jsx
@@ -7,7 +7,7 @@ const Reminder = () => {
             <h3>Mylanan 20 mg</h3>
             <h4>1 tablet after meal</h4>
                 <p>12 days</p>
-
+                <span className="circle"></span>
         </div>
     )
 } 
