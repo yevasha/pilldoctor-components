71ef71d68fdb101c60d6871e3bf8385f45156f4d
samina-shameem <samina.shameem.clt@gmail.com>
2024-10-02 14:05:17 +0200

    added medicin reminderstories.

---
+++ src/stories/Components/MedicineTracker/MedicineReminder.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MedicineReminder from "./MedicineReminder";
+
+export default {
+    title: "FRK/Components/PillCalender/MedicineReminder",
+    component: MedicineReminder,
+};
+
+// Default story
+export const Default = () => <MedicineReminder />;
