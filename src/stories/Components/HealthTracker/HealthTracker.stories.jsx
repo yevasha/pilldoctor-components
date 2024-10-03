6466d60b59f6120f6c65a06ae620d7740c5407d9
Calandra Leslie <calandra.leslie@gmail.com>
2024-10-03 19:28:32 +0200

    New Healthtracker update

---
+++ src/stories/Components/HealthTracker/HealthTracker.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HealthTracker from './HealthTracker';
+
+
+export default {
+  title: 'FRK/Components/HealthTracker/HealthTracker',
+  component: HealthTracker,
+};
+
+export const Default = () => <HealthTracker />;
+
+export const MenuWithLogin = () => <HealthTracker />;
