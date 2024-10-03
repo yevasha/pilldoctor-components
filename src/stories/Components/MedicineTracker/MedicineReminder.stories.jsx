39c2321a96e05dcddf1631e0c20f60ce8fb1b3e1
samina-shameem <samina.shameem.clt@gmail.com>
2024-10-03 14:30:53 +0200

    delet codes

--- src/stories/Components/MedicineTracker/MedicineReminder.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MedicineReminder from ".MedicineTracker/MedicineReminder"; // Adjust the path if necessary
-
-export default {
-    title: "FRK/Components/MedicineTracker/MedicineReminder",
-    component: MedicineReminder,
-};
-
-export const Default = () => <MedicineReminder />;
