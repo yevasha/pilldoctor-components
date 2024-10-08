82d5c701f249576d47eb711cde04143e402625d5
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-10-08 21:20:46 +0200

    updated indexes for builds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { Login } from "./stories/Components/Login";
+import { Register } from "./stories/Components/Register";
+import { Calendar } from "./stories/Components/PillCalendar";
+import { Homepage } from "./stories/Components/Home";
 
-export {Login};
+
+export {Login, Register, Calendar, Homepage};
---
+++ src/stories/Components/PillCalendar/index.js
@@ -0,0 +1,3 @@
+import Calendar from "./Calender";
+
+export {Calendar};
